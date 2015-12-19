d50ee25b0263e7aca5a64c27c9b20afe02d8bbcc
Tomasz Kuczma <tomaszkuczma@o2.pl>
2015-12-19 14:10:17 +0100

    positive path based on akka.become

--- src/main/kotlin/put/swn/threepc/Actor.kt
+++ src/main/kotlin/put/swn/threepc/Actor.kt
@@ -1,10 +1,8 @@
 package put.swn.threepc
 
-import akka.actor.ActorPath
+
+import akka.actor.Cancellable
 import akka.actor.UntypedActor
-import akka.pattern.Patterns
-import akka.util.Timeout
-import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
 
@@ -16,28 +14,81 @@ fun CommitSiteActorName(number: Int): String {
 }
 
 class CommitSiteActor(val id: Int, val size: Int) : UntypedActor() {
-    val t = Timeout(Duration.create(10, TimeUnit.SECONDS))
-    val actors = (0..size).filter { it != id }.map { context.system().actorFor("user/"+CommitSiteActorName(it)) }
+    val actors = (0..size).filter { it != id }.map { context.system().actorFor("user/" + CommitSiteActorName(it)) }
+    var counter = 0
+
+    init {
+        context.become { canCommit(it, null) }
+    }
 
     override fun onReceive(message: Any?) {
+    }
+
+    fun canCommit(message: Any?, timeout: Cancellable?) {
         if (message is StartMessage) {
-            //TODO Await and onFailure
-
-            val CanCommitRequestFutures = actors.map { Patterns.ask(it, CanCommit(), t) }
-            val CanCommitRequestResults = CanCommitRequestFutures.map { Await.result(it, t.duration()) }
-
-            val PreCommitRequestFutures = actors.map { Patterns.ask(it, PreCommit(), t) }
-            val PreCommitRequestResults = PreCommitRequestFutures.map { Await.result(it, t.duration()) }
-
-            val DoCommitRequestFutures = actors.map { Patterns.ask(it, DoCommit(), t) }
-            val DoCommitRequestResults = DoCommitRequestFutures.map { Await.result(it, t.duration()) }
-
-            println("Done")
-        } else if (message is Commit) {
-
+            println("CanCommit StartMessage")
+            actors.forEach { it.tell(CanCommit(), self) }
+            counter = 0
+            context.become { preCommit(it, timeout()) }
+        } else if (message is CanCommit) {
+            println("CanCommit CanCommit")
             sender.tell(Confirm(), self)
+            context.become { preCommit(it, timeout()) }
+        } else if (message is Confirm) {
+            println("CanCommit Confirm")
+            if (++counter == size - 1)
+                timeout?.cancel()
+        } else if (message is Abort) {
+            println("CanCommit Abort")
 
         }
     }
 
+    fun preCommit(message: Any, timeout: Cancellable) {
+        if (message is Confirm) {
+            println("PreCommit Confirm")
+            if (++counter == size - 1) {
+                timeout.cancel()
+
+                actors.forEach { it.tell(PreCommit(), self) }
+                counter = 0
+                context.become { doCommit(it, timeout()) }
+            }
+        } else if (message is Abort) {
+            println("PreCommit Abort")
+
+        } else if (message is PreCommit) {
+            println("PreCommit PreCommit")
+            timeout.cancel()
+
+            sender.tell(Confirm(), self)
+            context.become { doCommit(it, timeout()) }
+        }
+    }
+
+    fun doCommit(message: Any, timeout: Cancellable) {
+        if (message is Confirm) {
+            println("DoCommit Confirm")
+            if (++counter == size - 1) {
+                timeout.cancel()
+
+                actors.forEach { it.tell(DoCommit(), self) }
+                counter = 0
+                context.become { canCommit(it, timeout()) }
+            }
+        } else if (message is Abort) {
+            println("DoCommit Abort")
+
+        } else if (message is DoCommit) {
+            println("DoCommit DoCommit")
+            timeout.cancel()
+
+            sender.tell(Confirm(), self)
+            context.become { canCommit(it, null) }
+        }
+    }
+
+    fun timeout(): Cancellable {
+        return context.system().scheduler().scheduleOnce(Duration.create(5, TimeUnit.NANOSECONDS), self, Abort(), context.dispatcher(), null)
+    }
 }
