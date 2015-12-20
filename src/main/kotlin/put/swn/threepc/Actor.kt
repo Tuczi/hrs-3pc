7918f0316aaef01df0d8fc03b286c5c2f4d3e61c
Tomasz Kuczma <tomaszkuczma@o2.pl>
2015-12-20 16:58:17 +0100

    actor timeout fixed

--- src/main/kotlin/put/swn/threepc/Actor.kt
+++ src/main/kotlin/put/swn/threepc/Actor.kt
@@ -16,24 +16,33 @@ fun CommitSiteActorName(number: Int): String {
 class CommitSiteActor(val id: Int, val size: Int) : UntypedActor() {
     val actors = (0..size).filter { it != id }.map { context.system().actorFor("user/" + CommitSiteActorName(it)) }
     var counter = 0
+    var timeout: Cancellable? = null
 
     init {
-        context.become { canCommit(it, null) }
+        context.become { canCommit(it) }
     }
 
     override fun onReceive(message: Any?) {
     }
 
-    fun canCommit(message: Any?, timeout: Cancellable?) {
+    fun reset(timeout: Cancellable? = null) {
+        this.counter = 0
+        this.timeout = timeout
+    }
+
+    fun canCommit(message: Any?) {
         if (message is StartMessage) {
             println("CanCommit StartMessage")
             actors.forEach { it.tell(CanCommit(), self) }
-            counter = 0
-            context.become { preCommit(it, timeout()) }
+
+            reset(timeout())
+            context.become { preCommit(it) }
         } else if (message is CanCommit) {
             println("CanCommit CanCommit")
             sender.tell(Confirm(), self)
-            context.become { preCommit(it, timeout()) }
+
+            reset(timeout())
+            context.become { preCommit(it) }
         } else if (message is Confirm) {
             println("CanCommit Confirm")
             if (++counter == size - 1)
@@ -44,51 +53,53 @@ class CommitSiteActor(val id: Int, val size: Int) : UntypedActor() {
         }
     }
 
-    fun preCommit(message: Any, timeout: Cancellable) {
+    fun preCommit(message: Any) {
         if (message is Confirm) {
             println("PreCommit Confirm")
             if (++counter == size - 1) {
-                timeout.cancel()
+                timeout?.cancel()
 
                 actors.forEach { it.tell(PreCommit(), self) }
-                counter = 0
-                context.become { doCommit(it, timeout()) }
+                reset(timeout())
+                context.become { doCommit(it) }
             }
         } else if (message is Abort) {
             println("PreCommit Abort")
 
         } else if (message is PreCommit) {
             println("PreCommit PreCommit")
-            timeout.cancel()
+            timeout?.cancel()
 
             sender.tell(Confirm(), self)
-            context.become { doCommit(it, timeout()) }
+            reset(timeout())
+            context.become { doCommit(it) }
         }
     }
 
-    fun doCommit(message: Any, timeout: Cancellable) {
+    fun doCommit(message: Any) {
         if (message is Confirm) {
             println("DoCommit Confirm")
             if (++counter == size - 1) {
-                timeout.cancel()
+                timeout?.cancel()
 
                 actors.forEach { it.tell(DoCommit(), self) }
-                counter = 0
-                context.become { canCommit(it, timeout()) }
+                reset(timeout())
+                context.become { canCommit(it) }
             }
         } else if (message is Abort) {
             println("DoCommit Abort")
 
         } else if (message is DoCommit) {
             println("DoCommit DoCommit")
-            timeout.cancel()
+            timeout?.cancel()
 
-            sender.tell(Confirm(), self)
-            context.become { canCommit(it, null) }
+            sender.tell(Confirm(), self);
+            reset()
+            context.become { canCommit(it) }
         }
     }
 
     fun timeout(): Cancellable {
-        return context.system().scheduler().scheduleOnce(Duration.create(5, TimeUnit.NANOSECONDS), self, Abort(), context.dispatcher(), null)
+        return context.system().scheduler().scheduleOnce(Duration.create(5, TimeUnit.SECONDS), self, Abort(), context.dispatcher(), null)
     }
 }
